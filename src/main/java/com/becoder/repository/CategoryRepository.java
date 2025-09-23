09b1a4eff83c12c994ef0d8a71fc8c17af0e1f8d
sumitkprasad123 <sumitkprasad123@gmail.com>
2025-09-24 01:00:58 +0530

    implemented the category update functionality

--- src/main/java/com/becoder/repository/CategoryRepository.java
+++ src/main/java/com/becoder/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.becoder.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,8 @@ import com.becoder.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer>{
 
-	List<Category> findByIsActiveTrue();};
+	List<Category> findByIsActiveTrueAndIsDeletedFalse();
+
+	Optional<Category> findByIdAndIsDeletedFalse(Integer id);
+
+	List<Category> findByIsDeletedFalse();};
